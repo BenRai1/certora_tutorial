cccd0241a7dc4818bc1fe0b8b222641f70a30cf8
BenRai1 <benrainow@gmx.de>
2023-08-17 08:13:47 +0200

    Specs work with CVL2

--- 02.Lesson_InvestigateViolations/ERC20/runERC20.sh
+++ 02.Lesson_InvestigateViolations/ERC20/runERC20.sh
@@ -1,4 +1,4 @@
-certoraRun ERC20/ERC20Bug1.sol:ERC20 --verify ERC20:ERC20/ERC20.spec \
+certoraRun ERC20/ERC20Fixed.sol:ERC20 --verify ERC20:ERC20/ERC20.spec \
 --optimistic_loop \
 --send_only \
 # --solc solc8.0 \
--- 02.Lesson_InvestigateViolations/ERC20/runERC20Fixed.sh
+++ 02.Lesson_InvestigateViolations/ERC20/runERC20Fixed.sh
@@ -1,5 +1,5 @@
 solc-select use 0.8.0
-certoraRun ERC20Fixed.sol:ERC20 --verify ERC20:ERC20.spec \
+certoraRun ERC20/ERC20Fixed.sol:ERC20 --verify ERC20:ERC20/ERC20.spec \
 --optimistic_loop \
 --send_only \
 --msg "$1"
